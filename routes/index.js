c806f3b909b38f9646f03087cc9b210eb09fa4a2
Dan Zajdband <dan.zajdband@gmail.com>
2012-03-08 16:12:41 -0300

    home queries in utils

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@
 
 var everyauth = require('everyauth')
 	, mongoose = require('mongoose')
+	, utils = require('./utils')
 	, config = require('../config.json');
 
 /*
@@ -25,43 +26,19 @@ var app = module.parent.exports;
 
 require('./polls');
 
-var Poll = mongoose.model('Poll');
+var Poll = mongoose.model('Poll')
+	, User = mongoose.model('User');
 
 
 app.get('/', function(req, res){
-	function async_querys(queue, callback) {
-		var to_go = queue.length;
-		var results = [];
-		queue.forEach(function(query){
-			switch(query.type) {
-				case 'find':
-					qr = Poll.find(query.query);
-					break;
-				case 'findOne':
-					qr = Poll.findOne(query.query)
-					break;
-				default:
-					qr = false;
-			}
-			if(qr) {
-				if(query.desc) { qr.desc(query.desc); }
-				if(query.limit) { qr.limit(query.limit); }
-				qr.run(function(err, res) {
-					results[query.name] = res;
-					if (--to_go == 0) callback(results);
-				});
-			} else {
-				--to_go
-			}
-		});
-	};
+
 
 	querys = [
-		{ name: 'last_polls', type: 'find', query: {}, desc: 'updated_at', limit: 5 },
-		{ name: 'hot_poll', type: 'findOne', query: {}, desc: 'updated_at' }
+		{ name: 'last_polls', collection: Poll,  type: 'find', query: {}, desc: 'updated_at', limit: 5 },
+		{ name: 'hot_poll', collection: Poll, type: 'findOne', query: {}, desc: 'updated_at' }
 	];
 
-	async_querys(querys, function(context) {
+	utils.async_queries(querys, function(context) {
 		context.title = 'InstaPolls';
 		context.poll_domain = config.host.domain;
 		context.json_poll = JSON.stringify(context.hot_poll);
--- utils.js
+++ utils.js
@@ -14,3 +14,31 @@ var shasum = crypto.createHash('sha1');
 		shasum.update(Date.now().toString());
 		return shasum.digest('hex').substr(0,6);
 };
+
+exports.async_queries =	function (queue, callback) {
+		var to_go = queue.length;
+		var results = {};
+		queue.forEach(function(query){
+			var collection = query.collection;
+			switch(query.type) {
+				case 'find':
+					qr = collection.find(query.query);
+					break;
+				case 'findOne':
+					qr = collection.findOne(query.query)
+					break;
+				default:
+					qr = false;
+			}
+			if(qr) {
+				if(query.desc) { qr.desc(query.desc); }
+				if(query.limit) { qr.limit(query.limit); }
+				qr.run(function(err, res) {
+					results[query.name] = res;
+					if (--to_go == 0) callback(results);
+				});
+			} else {
+				--to_go
+			}
+		});
+	};
